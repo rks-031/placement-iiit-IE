bbf283a2e57abb399aecaac941845048381d2609
Md-Haris-085 <haris.iiitbh@gmail.com>
2024-10-13 03:07:52 +0530

    update new

---
+++ src/components/SQL/QuizComponent.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { questions, correctAnswers } from "./QuizData";
+import "../QuizComponent.css";
+
+
+
+const QuizComponent = () => {
+  const [selectedAnswers, setSelectedAnswers] = useState({});
+  const [feedback, setFeedback] = useState({});
+
+  const handleSelectAnswer = (question, answer) => {
+    setSelectedAnswers({ ...selectedAnswers, [question]: answer });
+  };
+
+  const handleSubmit = (question) => {
+    const isCorrect = selectedAnswers[question] === correctAnswers[question];
+    setFeedback({
+      ...feedback,
+      [question]: isCorrect
+        ? "Correct!"
+        : `Incorrect! The correct answer is ${correctAnswers[question]}.`,
+    });
+  };
+
+  return (
+    <div className="quiz-container">
+      {questions.map((question) => (
+        <div key={question.id} className="question-box">
+          {/* Render question text with image using dangerouslySetInnerHTML */}
+          <h3 dangerouslySetInnerHTML={{ __html: question.text }}></h3>
+          <div className="options">
+            {Object.keys(question.options).map((key) => (
+              <label key={key} className="option-label">
+                <input
+                  type="radio"
+                  name={question.id}
+                  onClick={() => handleSelectAnswer(question.id, key)}
+                />
+                {question.options[key]}
+              </label>
+            ))}
+          </div>
+          <button onClick={() => handleSubmit(question.id)}>
+            Check Answer
+          </button>
+          {feedback[question.id] && (
+            <p
+              className="feedback"
+              style={{
+                color: feedback[question.id].includes("Correct")
+                  ? "green"
+                  : "red",
+              }}
+            >
+              {feedback[question.id]}
+            </p>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default QuizComponent;
